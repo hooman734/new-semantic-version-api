00e657c373d108654ed50c79e775167168a5fba3
Hooman <hesamyan@outlook.com>
2020-06-13 16:33:12 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import json
+from helpers.retrieve_v3 import resolve_version
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return """
+    <div style="background-color:Tomato; color:white; text-align: center;">
+    <h1>Welcome to NuGet.org api.</h1>
+    <h2> <i> To use correctly, please request a path like: </i> </h2>
+    <h2> <mark>/ api / {package_name} / {major | minor | patch} / json</mark></h2>
+    <br />
+    <hr /> 
+    </div>
+    """
+
+
+@app.route('/api/<package_name>/<v_type>/json')
+def handle_version(package_name, v_type):
+    answer, code = resolve_version(package_name, v_type)
+    if code == '404':
+        return "404 not found."
+    else:
+        return json.dumps(answer)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
